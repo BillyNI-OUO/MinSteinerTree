0ff67bb174d3a929f47818747c455357baf36aa1
nibilly <nibilly118@gmail.com>
2023-06-03 18:34:44 +0800

    finish module & constants

---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+from .store import *
+from .repo import *
---
+++ src/constants.py
@@ -0,0 +1,15 @@
+from logging import DEBUG
+
+class constants:
+
+    LOG_FILE_NAME = 'app.log'
+    LOG_LEVEL = DEBUG
+    GREY_STYLE = "\x1b[38;20m"
+    YELLOW_STYLE = "\x1b[33;20m"
+    RED_STYLE = "\x1b[31;20m"
+    BOLD_RED_STYLE = "\x1b[31;1m"
+    RESET_STYLE = "\x1b[0m"
+    PLOT_X_SIZE = 10.0
+    PLOT_Y_SIZE = 10.0
+
+Constants = constants()
